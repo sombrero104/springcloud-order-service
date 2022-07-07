22c112a244e08209b89fa05f8a551c9e7bce1ad0
sombrero104 <sombrero.khy@gmail.com>
2022-07-07 21:10:44 +0900

    .

---
+++ src/main/java/me/service/OrderService.java
@@ -0,0 +1,10 @@
+package me.service;
+
+import me.dto.OrderDto;
+import me.jpa.OrderEntity;
+
+public interface OrderService {
+    OrderDto createOrder(OrderDto orderDetails);
+    OrderDto getOrderByOrderId(String orderId);
+    Iterable<OrderEntity> getOrdersByUserId(String userId);
+}
---
+++ src/main/java/me/vo/RequestOrder.java
@@ -0,0 +1,10 @@
+package me.vo;
+
+import lombok.Data;
+
+@Data
+public class RequestOrder {
+    private String productId;
+    private Integer qty;
+    private Integer unitPrice;
+}
